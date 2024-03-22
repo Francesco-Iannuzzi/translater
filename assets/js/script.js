618dc49f2c9f6185836bf46f6249279634451531
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2024-03-22 19:37:58 +0100

    select dome Element + add data to btn

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// select DOM element
+const inputText = document.querySelector('#text_to_translate');
+const langButtons = document.querySelectorAll('.lang_button');
+const resetButton = document.querySelector('.reset_button');
+
+// DOM translation
+const translationText = document.querySelector('.text_translate');
+const translationFlag = document.querySelector('.flag_tranlate');
